3f706bf3b1a34039750e6a792f01409575cf9ffc
achanova <60545894+achanova@users.noreply.github.com>
2020-02-01 16:37:24 +0200

    initial commit

---
+++ ProektiIf/ProektiIf/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ProektiIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uspeh = double.Parse(Console.ReadLine());
+            var dohod = double.Parse(Console.ReadLine());
+            if (uspeh>=5.50)
+            {
+                Console.WriteLine("Stipendia za otlichen uspeh");
+            }
+            else if (uspeh>=4.50 && dohod<=560)
+            {
+                Console.WriteLine("Socialna stipendia");
+            }
+            else
+            {
+                Console.WriteLine("Ne poluchava stipendia");
+            }
+        }
+    }
+}
